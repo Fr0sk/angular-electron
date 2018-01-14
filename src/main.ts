3f6a73a3908046106fb419b7abdae23ebe3ee1b1
Fr0sk <the.fr0sk@gmail.com>
2018-01-14 00:10:15 +0000

    Added angular-material, material-icons, nodemon and changed directories

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,71 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { app, BrowserWindow, screen } from 'electron';
+import * as fs from 'fs'
+import * as path from 'path';
 
-import { AppModule } from './app/app.module';
-import { environment } from 'environments';
+let win : Electron.BrowserWindow;
+let serve: boolean;
+const args = process.argv.slice(1);
+serve = args.some(val => val === '--serve');
+import * as url from 'url';
 
-if (environment.production) {
-  enableProdMode();
+if (serve) { require('electron-reload')(app.getAppPath(), {}) }
+
+function createWindow() {
+  const electronScreen = screen;
+  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    x: 0,
+    y: 0,
+    width: size.width,
+    height: size.height,
+  });
+  
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    protocol: 'file:',
+    pathname: path.join(app.getAppPath(), '/index.html'),
+    slashes:  true
+  }));
+
+  // Open the DevTools.
+  if (serve) { win.webContents.openDevTools(); }
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+try {
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow);
+
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+  });
+
+} catch (e) {
+  // Catch Error
+  // throw e;
+}
--- view/app/app.component.ts
+++ view/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ElectronService } from './providers/electron.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,12 @@ import { ElectronService } from './providers/electron.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public electronService: ElectronService) {
+  constructor(public electronService: ElectronService,
+    private translate: TranslateService) {
 
-    if (electronService.isElectron()) {
+    translate.setDefaultLang('en');
+
+    /*if (electronService.isElectron()) {
       console.log('Mode electron');
       // Check if electron is correctly injected (see externals in webpack.config.js)
       console.log('c', electronService.ipcRenderer);
@@ -17,6 +21,6 @@ export class AppComponent {
       console.log('c', electronService.childProcess);
     } else {
       console.log('Mode web');
-    }
+    }*/
   }
 }
